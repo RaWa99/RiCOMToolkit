7731c8d16e6d919f1838bb019efad98f31e269b2
Roy Walters <rawalters@gmail.ca>
2015-04-20 21:37:16 -0700

    cleanup, rename, and delete old files

--- PreMod/src/NullOutputnetCDF.f90
+++
@@ -1,8 +0,0 @@
-!***************************************************************
- subroutine Output_netCDF
-!
-!	Create netCDF file to contain Premod output
-!	and write off the data
-
-end subroutine
-       
